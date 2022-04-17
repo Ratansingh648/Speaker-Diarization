bcb424f961887c2bb871960dfbd28fae81042cd3
Ratansingh648 <ratansingh648@gmail.com>
2022-04-17 01:56:07 -0400

    Correcting Speaker Diarisation

--- VAD.py
+++ VAD.py
@@ -1,3 +1,4 @@
+import librosa
 import numpy as np
 from scipy.ndimage.morphology import binary_dilation
 import struct
@@ -5,22 +6,22 @@ import webrtcvad
 
 INT16_MAX = (2**15)-1
 
+
 class VAD(object):
 
-    def __init__(self, window_length=30, moving_average_width=8, max_silence_length=6):
+    def __init__(self, window_length=30, moving_average_width=8, max_silence_length=6, base_sampling_frequency=16000):
         self.vad_window_length = window_length
         self.vad_moving_average_width = moving_average_width
         self.vad_max_silence_length = max_silence_length
-
+        self.base_sampling_frequency = base_sampling_frequency
 
     def _moving_average(self, array, width):
         """Smoothing Voice Detectoion with moving average"""
-        array_padded = np.concatenate((np.zeros((width - 1) // 2 ), array, np.zeros(width // 2)))
+        array_padded = np.concatenate((np.zeros((width - 1) // 2), array, np.zeros(width // 2)))
         cumulative_sum = np.cumsum(array_padded, dtype=float)
         cumulative_sum[width:] -= cumulative_sum[:-width]
         return cumulative_sum[width-1:]/width
 
-    
     def _normalize_volume(self, wav, target_dbfs, increase_only=False, decrease_only=False):
         """Sets the volume of audio file to certain target DB Level"""
         if increase_only and decrease_only:
@@ -30,24 +31,26 @@ class VAD(object):
         wav_dbfs = 20*np.log10(rms/INT16_MAX)
         dbfs_change = target_dbfs - wav_dbfs
 
-        if (dbfs_change < 0  and increase_only) or (dbfs_change > 0 and decrease_only):
+        if (dbfs_change < 0 and increase_only) or (dbfs_change > 0 and decrease_only):
             return wav
         return wav * (10 ** (dbfs_change/20))
 
-
-    def detect_voice(self, wav, sampling_frequency, aggression_mode=3, trim = False):
+    def detect_voice(self, wav, sampling_frequency, aggression_mode=3, trim=False):
         """Detects the segments of voice"""
+
+        wav = self._normalize_volume(wav, -30, increase_only=True)
+        wav = librosa.resample(wav, sampling_frequency, self.base_sampling_frequency)
+        sampling_frequency = self.base_sampling_frequency
         samples_per_window = (self.vad_window_length*sampling_frequency)//1000
         wav = wav[:len(wav) - (len(wav) % samples_per_window)]
         pcm_wav = struct.pack("%dh" % len(wav), *(np.round(wav * INT16_MAX)).astype(np.int16))
 
         voice_flags = []
         vad = webrtcvad.Vad(mode=aggression_mode)
-        
         # Creating voice windows
         for window_start in range(0, len(wav), samples_per_window):
             window_end = window_start + samples_per_window
-            voice_flags.append(vad.is_speech(pcm_wav[window_start*2 : window_end*2], sampling_frequency))
+            voice_flags.append(vad.is_speech(pcm_wav[window_start*2: window_end*2], sample_rate=sampling_frequency))
         voice_flags = np.array(voice_flags)
 
         # Running Moving average to Smooth the region
--- main.py
+++ main.py
@@ -1,37 +1,73 @@
-from fileinput import filename
+import librosa
+import matplotlib.pyplot as plt
 import numpy as np
 import os
-from scipy.io.wavfile import read
-import matplotlib.pyplot as plt
+import sounddevice as sd
+from spectralcluster import SpectralClusterer, RefinementOptions
 
 from VAD import VAD
 from VoiceEncoder import VoiceEncoder
 
+
+def create_labelling(labels, wav_splits):
+    times = [((s.start + s.stop) / 2) / 16000 for s in wav_splits]
+    labelling = []
+    start_time = 0
+
+    for i, time in enumerate(times):
+        if i > 0 and labels[i] != labels[i-1]:
+            temp = [str(labels[i-1]), start_time, time]
+            labelling.append(tuple(temp))
+            start_time = time
+        if i == len(times)-1:
+            temp = [str(labels[i]), start_time, time]
+            labelling.append(tuple(temp))
+
+    return labelling
+
+
+def create_label_array(audio_mask, labelling, sample_len):
+    array_sample = np.array([-1]*sample_len)
+    label_array = []
+    for label, start, end in labelling:
+        label_array.extend([label]*int(sampling_frequency*(end-start)))
+    label_array.extend([label]*(sum(audio_mask)-len(label_array)))
+    array_sample[audio_mask] = label_array
+    array_sample = np.multiply(audio_mask, array_sample)
+    return array_sample
+
+
 if __name__ == "__main__":
     filename = "audio_with_noise.wav"
-    #filename = "X2zqiX6yL3I.wav"
-    sampling_frequency, wav = read(os.path.join("data", filename))
-    
+    # filename = "X2zqiX6yL3I.wav"
+
+    wav, sampling_frequency = librosa.load(os.path.join("data", filename), sr=None)
+
     vad = VAD()
     audio_mask, wav = vad.detect_voice(wav, sampling_frequency)
-    """
-    fig1 = plt.figure(1)
-    plt.plot(wav)
-    plt.show()
+    sample_len = len(wav)
+    wav2 = wav
+    wav = wav[audio_mask]
 
-    fig2 = plt.figure(2)
-    plt.plot(np.multiply(audio_mask, wav))  
-    plt.show()
-    """
     encoder = VoiceEncoder("cpu")
 
-    segments = [[0, 5.5], [6.5, 12], [17, 25]]
-    speaker_names = ["Kyle Gass", "Sean Evans", "Jack Black"]
-    speaker_wavs = [wav[int(s[0] * sampling_frequency):int(s[1] * sampling_frequency)] for s in segments]
-
     _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16, min_coverage=0.75)
-    speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
-    print(np.array(speaker_embeds).shape)
-    print(len(audio_mask))
-    print(np.array(cont_embeds).shape)
-    print(np.array(wav_splits).shape)
+    # speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
+
+    refinement_options = RefinementOptions(
+        gaussian_blur_sigma=1,
+        p_percentile=0.90)
+
+    clusterer = SpectralClusterer(
+        min_clusters=2,
+        max_clusters=100,
+        refinement_options=refinement_options)
+
+    labels = clusterer.predict(cont_embeds)
+    labelling = create_labelling(labels, wav_splits)
+
+    labeled = create_label_array(audio_mask, labelling, sample_len)
+    plt.plot(np.array(range(len(audio_mask)))/16000, -(labeled+1))
+    plt.plot(np.array(range(len(audio_mask)))/16000, audio_mask, "r--")
+    plt.plot(np.array(range(len(audio_mask)))/16000, wav2, color="black")
+    plt.show()
